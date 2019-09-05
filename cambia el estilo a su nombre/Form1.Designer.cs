241992598d13c3eb67dfadee38f40d59abfef717
cristian ermes morales loaiza <christianjack01@hotmail.com>
2019-09-05 01:48:42 -0500

    cambia el estilo de su nombre con un boton

---
+++ cambia el estilo a su nombre/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace cambia_el_estilo_a_su_nombre
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblnombre = new System.Windows.Forms.Label();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnpresione = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.AutoSize = true;
+            this.lblnombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblnombre.Location = new System.Drawing.Point(373, 132);
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(0, 13);
+            this.lblnombre.TabIndex = 0;
+            this.lblnombre.Click += new System.EventHandler(this.Lblnombre_Click);
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(236, 136);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(100, 20);
+            this.txtnombre.TabIndex = 1;
+            this.txtnombre.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(27, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(203, 24);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Digite Su Nombre Aqui";
+            // 
+            // btnpresione
+            // 
+            this.btnpresione.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnpresione.Location = new System.Drawing.Point(164, 207);
+            this.btnpresione.Name = "btnpresione";
+            this.btnpresione.Size = new System.Drawing.Size(185, 69);
+            this.btnpresione.TabIndex = 2;
+            this.btnpresione.Text = "presione aqui";
+            this.btnpresione.UseVisualStyleBackColor = true;
+            this.btnpresione.Click += new System.EventHandler(this.Btnpresione_Click);
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnsalir.Location = new System.Drawing.Point(399, 206);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(135, 70);
+            this.btnsalir.TabIndex = 3;
+            this.btnsalir.Text = "salir";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            this.btnsalir.Click += new System.EventHandler(this.Btnsalir_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(153, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(367, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "programa para ver tu nombre en otro estilo";
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnlimpiar.Location = new System.Drawing.Point(606, 207);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(117, 69);
+            this.btnlimpiar.TabIndex = 5;
+            this.btnlimpiar.Text = "limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = true;
+            this.btnlimpiar.Click += new System.EventHandler(this.Btnlimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.btnpresione);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblnombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblnombre;
+        private System.Windows.Forms.TextBox txtnombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnpresione;
+        private System.Windows.Forms.Button btnsalir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnlimpiar;
+    }
+}
+
---
+++ cambia el estilo a su nombre/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace cambia_el_estilo_a_su_nombre
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+
+
+
+        }
+
+        private void Btnpresione_Click(object sender, EventArgs e)
+        {
+            lblnombre.Text = txtnombre.Text;
+
+
+            using (Font font = new Font("Constantia", 30.0f))
+            {
+                lblnombre.Font = font;
+                lblnombre.ForeColor = Color.Green;
+            }
+
+        }
+
+        private void Lblnombre_Click(object sender, EventArgs e)
+        {
+         
+
+        }
+
+        private void Btnsalir_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("gracias por utilizar mi programa hasta la prixima-- att: jack01");
+            this.Close();
+        }
+
+        private void Btnlimpiar_Click(object sender, EventArgs e)
+        {
+            lblnombre.Text = "";
+            txtnombre.Text = "";
+        }
+    }
+}
